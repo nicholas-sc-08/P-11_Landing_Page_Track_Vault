77e081b3cc4a22df0337e0cfbaca7fa25c4e0eb3
nicholas-sc-08 <nicholassc.2008@gmail.com>
2025-01-19 15:00:41 -0300

    Término do projeto

--- Landing Page Track Vault/src/router/routes.jsx
+++
@@ -1,9 +0,0 @@
-import { createBrowserRouter } from "react-router-dom"
-import Pagina_inicial from "../pages/Pagina_inicial";
-
-const router = createBrowserRouter([
-    
-    { path: "/", element: <Pagina_inicial /> },   
-])
-
-export default router;
